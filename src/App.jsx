3b6bbde8215e95b6d2465d2fb20811209dfd6c4d
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-06-18 13:43:00 -0400

    Basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
-import React from "react";
 import { createRoot } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  //function component must return markup
-  //that's what React.createElement generates
-  return React.createElement(
-    //creates one instance of some component or tag
-    "div",
-    //atributes we're pasing to the tag or component
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        animal: "Dog",
-        name: "Luis",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        animal: "Bird",
-        name: "Pepper",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, {
-        animal: "Cat",
-        name: "Doink",
-        breed: "Mixed",
-      }),
-    ]
-  );
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="dog" breed="Havanese" />
+    <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+    <Pet name="Doink" animal="cat" breed="Mixed" />
+  </div>;
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
