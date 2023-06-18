087cb7928787e3cdb75187065662cb53de331b49
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-06-18 14:06:59 -0400

    Add search input for location using hooks

---
+++ src/SearchParams.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="location"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
